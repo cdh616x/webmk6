deab94df2e6174068099f46549eefde17d9826b8
cdh666x <91150236+cdh616x@users.noreply.github.com>
2021-11-29 18:54:55 -0500

    added things

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,14 +3,14 @@ import AboutPic from "../images/me1.png"
 
 export default function About() {
   return (
-    <div class="container-fluid">
-      <div class="row d-flex flex-wrap align-items-center">
-        <div class="col-lg-6 col-sm-6">
-          <h2 class="jane">A little bit about me...</h2>
-          <h5>Goodness, where to even begin... I am a twenty-something year old web designer, musician, nature-lover, and so much more. I have many diverse interests, including pre-history, space and the physics that were used to slap this universe together, music, and many more. If I am not programming, I am either making music, watching documentaries or hockey or football, or spending time with family and friends or plants and animals.</h5>
+    <div className="container-fluid">
+      <div className="row d-flex flex-wrap align-items-center">
+        <div className="col-lg-6 col-sm-6">
+          <h2 className="jane">A little bit about me...</h2>
+          <h5 className="about-text">Goodness, where to even begin... I am a twenty-something year old web designer, musician, nature-lover, and so much more. I have many diverse interests, including pre-history, space and the physics that were used to slap this universe together, music, and many more. If I am not programming, I am either making music, watching documentaries or hockey or football, or spending time with family and friends or plants and animals.</h5>
         </div>
-        <div class="col-lg-6 col-sm-6">
-          <img class="crossing img-fluid" src={AboutPic} alt="" />
+        <div className="col-lg-6 col-sm-6">
+          <img className="mx-auto d-block" src={AboutPic} alt="" />
         </div>
       </div>
     </div>
