6b1527eb758397fc7a2157efe8a355d9fd5ae0c9
cdh616x <91150236+cdh616x@users.noreply.github.com>
2021-12-02 23:46:04 -0500

    new build
    updated navbar picture with something less deathy / updated build... will update build for every commit from now on

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
-import Skull from "../../src/images/Skull1.png"
+import Skull from "../../src/images/cdh.xcf"
 
 export default function Navbar() {
   return (
