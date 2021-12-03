5cbcb44b789be6c8ae9943450a0410703ac5acbb
cdh616x <91150236+cdh616x@users.noreply.github.com>
2021-12-02 23:57:07 -0500

    updated navbar to reflect web domain name

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light bg-#062743" style={{backgroundColor: "#B91646", fontFamily: "Enigmatic, sans serif"}}>
       <Link className="navbar-brand" to="/">
           <img src={Skull} height="50" width="50" alt="mnm" />
-          LZ Labs
+          CDH Create
       </Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
