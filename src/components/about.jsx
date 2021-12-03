a603fa59f8927828d08a8cf449587f08f8ed39fc
cdh616x <91150236+cdh616x@users.noreply.github.com>
2021-12-02 22:56:21 -0500

    new build
    changed some of the wording in the about component / updated build file

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,7 @@ export default function About() {
       <div className="row d-flex flex-wrap align-items-center">
         <div className="col-lg-6 col-sm-6">
           <h2 className="jane">A little bit about me...</h2>
-          <h5 className="about-text">Goodness, where to even begin... I am a twenty-something year old web designer, musician, nature-lover, and so much more. I have many diverse interests, including pre-history, space, physics and natural sciences, music, and more! If I am not programming, I am either making music, watching documentaries or hockey or football, or spending time with family and friends or plants and animals.</h5>
+          <h5 className="about-text">Goodness, where to even begin... I am a twenty-something year old web designer, musician, nature-lover, and so many other things. I have many diverse interests, including pre-history, space, physics and natural sciences, music, and more! If I am not programming, I am either making music, watching documentaries or hockey or football, or spending time with family and friends or plants and animals.</h5>
         </div>
         <div className="col-lg-6 col-sm-6">
           <img className="mx-auto d-block" src={AboutPic} alt="" />
