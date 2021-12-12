9fc0a533d78b7a688e2420b917e9dbc6e9e732b9
cdh616x <91150236+cdh616x@users.noreply.github.com>
2021-12-11 22:18:02 -0500

    new build
    added new project to project page

--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -7,6 +7,7 @@ import Navbar from "../../src/components/navbar.jsx";
 import Andy from "../images/andy-site.png";
 import Ski from "../images/sss.png";
 import Drum from "../images/drumkit.png";
+import Grower from "../images/grower.png";
 
 export default function Projects() {
   return(
@@ -17,6 +18,9 @@ export default function Projects() {
         <h1>Projects</h1>
         <p>
           <a href="https://cdh616x.github.io/andy/" target="_blank">Andy Shaw Illustrations</a>
+        </p>
+        <p>
+          <a href="https://cdh616x.github.io/grower/" target="_blank">Grower's Edge</a>
         </p>
         <p>
           <a href="https://cdh616x.github.io/skis-sub-shop/" target="_blank">Ski's Sub Shop</a>
@@ -33,6 +37,14 @@ export default function Projects() {
         </a>
         <a href="https://cdh616x.github.io/andy/" target="_blank" rel="noreferrer">
           <img className="img-fluid" src={Andy} alt="" />
+        </a>
+      </div>
+      <div class="container-fluid">
+        <a href="https://github.com/cdh616x/grower" target="_blank" rel="noreferrer">
+          <h3><i className="fab fa-github-square"><h5>Source Code</h5></i></h3>
+        </a>
+        <a href="https://cdh616x.github.io/grower/" target="_blank">
+          <img className="img-fluid" src={Grower} alt="" />
         </a>
       </div>
       <div class="container-fluid">
