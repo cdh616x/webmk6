9d115dcfb667bff978b504750f393a2855bb5d74
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-01-04 14:50:10 -0500

    new build
    added Emily MERN stack to projects page and reformatted some components on that page

--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -17,6 +17,9 @@ export default function Projects() {
       <div className="container-fluid proj">
         <h1>Projects</h1>
         <p>
+          <a href="https://afternoon-mountain-72202.herokuapp.com/auth/google" target="_blank">Emily - MERN stack app (in progress...)</a>
+        </p>
+        <p>
           <a href="https://cdh616x.github.io/andy/" target="_blank">Andy Shaw Illustrations</a>
         </p>
         <p>
@@ -31,6 +34,9 @@ export default function Projects() {
       </div>
     </section>
     <section id="img">
+      <div className="container-fluid mern">
+        <h3>The full stack app (which uses the MERN stack) is a work in progress, but can still be visited to see progress that I have made. Basically, it will have you sign in using Google OAuth. Once that happens, little will occur (I said it was a work in progress!), but you can visit https://afternoon-mountain-72202.herokuapp.com/api/current_user which will display some banal items from your Google profile!</h3>
+      </div>
       <div className="container-fluid">
         <a href="https://github.com/cdh616x/andy" target="_blank">
           <h3><i className="fab fa-github-square"><h5>Source Code</h5></i></h3>
