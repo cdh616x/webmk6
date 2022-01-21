a6207afac263a4588a56d495c4455f7d9764af9b
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-01-21 12:33:18 -0500

    new build

--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -52,7 +52,7 @@ export default function Projects() {
         <a href="https://github.com/cdh616x/grower-react" target="_blank" rel="noreferrer">
           <h3><i className="fab fa-github-square"><h5>Source Code</h5></i></h3>
         </a>
-        <a href="https://cdh616x.github.io/grower/" target="_blank" rel="noreferrer">
+        <a href="https://cdh616x.github.io/grower-react/" target="_blank" rel="noreferrer">
           <img className="img-fluid" src={Grower} alt="" />
         </a>
       </div>
