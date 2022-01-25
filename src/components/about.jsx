da4451bad0f30e94eaf9627355e942dceb71f2fa
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-01-25 13:48:11 -0500

    new build
    fixed a typo courtesy of Benda

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,7 +12,7 @@ export default function About() {
         <div className="col-lg-6 col-sm-12">
           <h2 className="jane">A little bit about me...</h2>
           <h5 className="about-text">Goodness, where to even begin... I am a twenty-something year old web designer, musician, nature-lover, and so many other things. I have many diverse interests, including pre-history, space, physics and natural sciences, music, and more! If I am not programming, I am either making music, watching documentaries or hockey or football, or spending time with family and friends or plants and animals.</h5>
-          <h5 className="about-text">As far as software development goes, lately I have been focusing on user-interface design and user experience. I am sharpening my skills to make truly beautiful websites with proprietary code. I am also learning backend processes and framworks, like Node.js and Express! If you have an idea for a project, I would love to hear about it!</h5>
+          <h5 className="about-text">As far as software development goes, lately I have been focusing on user-interface design and user experience. I am sharpening my skills to make truly beautiful websites with proprietary code. I am also learning backend processes and frameworks, like Node.js and Express! If you have an idea for a project, I would love to hear about it!</h5>
         </div>
         <div className="col-lg-6 col-sm-12">
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
