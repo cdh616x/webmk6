df905be8505544d8f7bbd6d1a3d17efae31b21a6
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-02-27 12:14:13 -0500

    edited to make function on local computer again

--- src/server.js
+++ src/server.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
+const port = process.env.PORT || 5000;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(port, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
