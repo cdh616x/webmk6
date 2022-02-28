def06865246f5c86d83d7b52e4ec486ef2082c1e
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-02-28 12:34:30 -0500

    testing

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -13,7 +13,7 @@ export default function ContactForm() {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://pa3xwrbso9.execute-api.us-east-1.amazonaws.com/default/webmk6-email", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
